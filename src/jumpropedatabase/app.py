9fc93aff4cf7bb606fc10e3b46ed1df58a039c4e
Hayden Thomas <haydenthomasjump@gmail.com>
2023-10-30 21:07:01 -0400

    Update app basics

--- src/jumpropedatabase/app.py
+++ src/jumpropedatabase/app.py
@@ -2,8 +2,11 @@
 Create a database for jump rope tournaments
 """
 import toga
+from os import path
+from os import mkdir, rmdir
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+from .utilities.get_data_path import get_data_path, find_databases
 
 
 class JumpRopeDatabase(toga.App):
@@ -16,12 +19,49 @@ class JumpRopeDatabase(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
+        
+        # determine if datapath exists
+        valid_system, system_data_path = get_data_path()
+        
+        # Initialize main window
         main_box = toga.Box()
 
         self.main_window = toga.MainWindow(title=self.formal_name)
+        self.system_data_path = system_data_path
         self.main_window.content = main_box
+        
+        if valid_system and path.isdir(system_data_path):
+            self.main_app()
+        elif valid_system and not path.isdir(system_data_path):
+            install_button = toga.Button("Install", on_press=self.create_directory)
+            install_button.style.update(width=100)
+            main_box.add(install_button)
+            main_box.style.update(direction=COLUMN, padding=100,
+                                  alignment="right")
+        else:
+            None
+        
         self.main_window.show()
+    
+    def create_directory(self, widget):
+        mkdir(self.system_data_path)
+        widget.style.update(visibility="hidden")
+        self.main_app()
+    
+    def main_app(self):
+        # get the main container
+        main_box = self.main_window.content
+        
+        # find databases that exist
+        databases = find_databases(self.system_data_path)
+        db_dropdown = toga.Selection(items=databases)
+        main_box.add(db_dropdown)
 
+        # add the uninstall button in bottom right
+        
+        
+        
 
 def main():
     return JumpRopeDatabase()
+
--- src/jumpropedatabase/utilities/get_data_path.py
+++ src/jumpropedatabase/utilities/get_data_path.py
@@ -5,7 +5,7 @@ Created on Mon Mar 20 17:32:29 2023
 @author: thomahm1
 """
 
-from os import path
+from os import path, listdir
 from platform import system
 
 def get_data_path():
@@ -26,13 +26,36 @@ def get_data_path():
     system_data_path = str()
     if system().find("Windows") > -1:
         valid_system = True 
-        system_data_path = 'C:\\Program Files (x86)\\jump rope database\\'
+        system_data_path = 'C:\\Program Files (x86)\\jump_rope_database\\'
     elif system().find("Darwin") > -1:
         valid_system = True
-        system_data_path = '~/Library/Application Support/jump rope database'
+        root_dir = path.expanduser("~")
+        system_data_path = root_dir + '/Library/Application Support/jump_rope_database'
     else:
         # immediately return False and empty string
         return valid_system, system_data_path
     
     
     return valid_system, system_data_path
+
+def find_databases(database_path:str):
+    '''
+    
+
+    Parameters
+    ----------
+    database_path : str
+        Path to the databases looking specifically for .db files
+
+    Returns
+    -------
+    databases : list
+        list of all database files
+
+    '''
+    
+    
+    
+    databases=[]
+    print(database_path)
+    return databases
